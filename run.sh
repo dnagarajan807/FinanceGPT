3b4ea2efeb9ab1f4839f33aa801867ad86fd4d97
dnagarajan807 <dnagarajan807@gmail.com>
2025-10-19 12:26:17 -0400

    new commit

--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export OPENAI_API_KEY=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
-uvicorn app.main:app --host 0.0.0.0 --port 8000
